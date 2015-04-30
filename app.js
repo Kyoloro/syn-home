17d3a37679fc98eb03313e7c6ab4e1f67e88652a
Nekron <nekron.hyt@gmail.com>
2015-04-30 12:27:18 +0800

    catch error
    uncaught error happened

--- app.js
+++ app.js
@@ -10,11 +10,6 @@ var express = require('express'),
 
 var app = express();
 
-app.use(morgan('dev', {
-    skip: function (req, res) {
-        return res.statusCode < 400
-    }
-}));
 app.use(compression());
 app.use(favicon(__dirname + '/public/img/favicon.ico',{
     maxAge:'30d'
@@ -25,6 +20,7 @@ app.use(express.static(__dirname + '/public', {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.engine('jade', jade.__express);
+app.use(morgan('dev'));
 app.use(methodOverride());
 
 if (!config.debug) {
